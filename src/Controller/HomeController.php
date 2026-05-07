8e5d900467d4e150a004e1c06b44fcb60b33aec7
Tinashe Makarudze <73774313+TinasheMK@users.noreply.github.com>
2026-05-07 14:49:50 +0200

    Add testimonial template with structured layout and sample content

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,17 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
-use Twig\Environment;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class HomeController extends AbstractController
 {
-
-    public function __construct(Environment $twig)
-    {
-        $this->loader = $twig->getLoader();
-    }
+    private const PAGES = [
+        'mission' => 'mission.html.twig',
+        'services' => 'services.html.twig',
+        'spaces' => 'spaces.html.twig',
+        'partners' => 'partners.html.twig',
+        'contact' => 'contact.html.twig',
+    ];
 
     #[Route('/', name: 'home')]
     public function index(): Response
@@ -23,15 +22,13 @@ class HomeController extends AbstractController
         return $this->render('index.html.twig');
     }
 
-    #[Route('/{path}')]
-    public function root($path)
+    #[Route('/{path}', name: 'page')]
+    public function page(string $path): Response
     {
-        if ($this->loader->exists($path.'.html.twig')) {
-            if ($path == '/' || $path == 'home') {
-                die('Home');
-            }
-            return $this->render($path.'.html.twig');
+        if (isset(self::PAGES[$path])) {
+            return $this->render(self::PAGES[$path]);
         }
+
         throw $this->createNotFoundException();
     }
 }
